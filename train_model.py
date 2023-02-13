1dd0c07abecba86407382073f79d43399249366c
ronTohar1 <rontoh@post.bgu.ac.il>
2023-02-13 21:57:03 +0200

    trainings

--- train_model.py
+++ train_model.py
@@ -8,12 +8,14 @@ import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import cross_val_predict, cross_val_score, KFold
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
-
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
 
 def load_model():
     with open(MODEL_PATH, "rb") as f:
         model = pickle.load(f)
+    return model
+
 
 def save_model(model):
     with open(MODEL_PATH, "wb") as f:
@@ -29,36 +31,36 @@ def train_model():
     X = df.drop("label", axis=1)
     y = df["label"]
 
+
+
     # Define the model
-    mlp = MLPClassifier(hidden_layer_sizes=(20,20), max_iter=500, alpha=0.0001,
-                        solver='sgd', verbose=10, random_state=21,tol=0.000000001)
+    mlp = MLPClassifier(max_iter=200, solver='sgd', verbose=10, random_state=21, tol=0.000000001)
+
+    # Define the hyperparameters to search
+    param_grid = {'hidden_layer_sizes': [(20, 20), (30, 30), (40, 40), (10,10), (5,5)],
+                'alpha': [0.0001, 0.001, 0.01]}
 
     # Define the k-fold cross-validation
     kfold = KFold(n_splits=5)
 
-    # Perform k-fold cross-validation and get predictions
-    predictions = cross_val_predict(mlp, X, y, cv=kfold)
+    # Define a scoring function based on accuracy
+    scorer = make_scorer(accuracy_score)
 
-    # Calculate accuracy, recall, precision, and f1 score
-    acc = accuracy_score(y, predictions)
-    recall = recall_score(y, predictions, average='weighted')
-    precision = precision_score(y, predictions, average='weighted')
-    f1 = f1_score(y, predictions, average='weighted')
+    # Use GridSearchCV to search for the best hyperparameters
+    grid_search = GridSearchCV(mlp, param_grid, scoring=scorer, cv=kfold)
+    grid_search.fit(X, y)
 
-    # Print the results
-    print("Accuracy:", acc)
-    print("Recall:", recall)
-    print("Precision:", precision)
-    print("F1 Score:", f1)
+    # Get the best parameters
+    best_parameters = grid_search.best_params_
 
-    
+    # Train the model with the best parameters
+    best_mlp = MLPClassifier(max_iter=200, solver='sgd', verbose=10, random_state=21, tol=0.000000001, 
+                            hidden_layer_sizes=best_parameters['hidden_layer_sizes'],
+                            alpha=best_parameters['alpha'])
 
-    # Plot the results
-    plt.plot(['accuracy', 'recall', 'precision', 'f1_score'], [acc, recall, precision, f1])
-    plt.xlabel('Metric')
-    plt.ylabel('Score')
-    plt.title('Neural Network Performance')
-    plt.show()
+    save_model(best_mlp)
+
+
 
 
 
